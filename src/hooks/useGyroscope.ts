8846b931151df3e087547c5769604e8c07468df2
takashi0602 <t.takashi0602@gmail.com>
2022-09-25 05:06:27 +0900

    feat: ジオロスコープの取得間隔を3ミリ秒にする

--- src/hooks/useGyroscope.ts
+++ src/hooks/useGyroscope.ts
@@ -23,6 +23,7 @@ export const useGyroscope = () => {
   };
 
   useEffect(() => {
+    Gyroscope.setUpdateInterval(300);
     subscribe();
     return () => unsubscribe();
     // eslint-disable-next-line react-hooks/exhaustive-deps
