a6e6de8986ef20ae71fcdd8d34a02eb80daf5d8a
takashi0602 <t.takashi0602@gmail.com>
2022-09-25 05:05:23 +0900

    fix: axiosClient の修正

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,11 +1,18 @@
 import { API_URL, UYUNPUNION_TOKEN } from '@env';
 import axios from 'axios';
 
-export const axiosClient = axios.create({
+const axiosClient = axios.create({
   baseURL: API_URL,
   timeout: 3000,
-  headers: {
+});
+
+axiosClient.interceptors.request.use((config) => {
+  config.headers = {
     'Content-Type': 'application/json',
     'UYUNPUNION-TOKEN': UYUNPUNION_TOKEN,
-  },
+  };
+
+  return config;
 });
+
+export { axiosClient };
