3ed41789696d8026a286b4dbb5bdef21de0441d3
takashi0602 <t.takashi0602@gmail.com>
2022-09-25 06:26:52 +0900

    feat: start/stopを分ける

--- src/hooks/useLoyly.ts
+++ src/hooks/useLoyly.ts
@@ -5,16 +5,40 @@ import { axiosClient } from '../utils/axiosClient';
 export const useLoyly = () => {
   const requested = useRef<boolean>(false);
 
+  const startLoyly = useCallback(async () => {
+    try {
+      await axiosClient.post<{ message: string }>(`humidifier/start`);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(error.message);
+        return;
+      }
+      console.error('エラーが発生しました。');
+    }
+  }, []);
+
+  const stopLoyly = useCallback(async () => {
+    try {
+      await axiosClient.post<{ message: string }>(`humidifier/stop`);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(error.message);
+        return;
+      }
+      console.error('エラーが発生しました。');
+    }
+  }, []);
+
   const postLoyly = useCallback(async () => {
     try {
       if (requested.current) return;
       requested.current = true;
-      await axiosClient.post<{ message: string }>(`humidifier/start`);
+      await startLoyly();
       console.log('start');
 
       // 10秒後に止める
       setTimeout(async () => {
-        await axiosClient.post<{ message: string }>(`humidifier/stop`);
+        await stopLoyly();
         requested.current = false;
         console.log('stop');
       }, 10000);
@@ -25,9 +49,11 @@ export const useLoyly = () => {
       }
       console.error('エラーが発生しました。');
     }
-  }, []);
+  }, [startLoyly, stopLoyly]);
 
   return {
+    startLoyly,
+    stopLoyly,
     postLoyly,
   };
 };
