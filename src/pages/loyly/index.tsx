b47e8ba67ae7193e487c2e7acfe40d71e9630d0e
takashi0602 <t.takashi0602@gmail.com>
2022-09-24 19:29:11 +0900

    feat: ボタンにreact native elementsを使用する

--- src/pages/loyly/index.tsx
+++ src/pages/loyly/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { StackNavigator } from '../../types/stackNavigator';
+import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,12 +12,9 @@ const styles = StyleSheet.create({
 });
 
 export const Loyly = () => {
-  const navigation = useNavigation<NativeStackNavigationProp<StackNavigator>>();
-
   return (
     <View style={styles.container}>
       <Text>ロウリュページ</Text>
-      <Button title="トップへ" onPress={() => navigation.goBack()} />
       <StatusBar style="auto" />
     </View>
   );
