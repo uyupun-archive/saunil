af5238484974aee9f678c436a241a19e1b76acb8
takashi0602 <t.takashi0602@gmail.com>
2022-09-25 12:23:24 +0900

    feat: startに合わせて音を流し、stopに合わせて音を止める

--- src/hooks/useAufguss.ts
+++ src/hooks/useAufguss.ts
@@ -2,13 +2,17 @@ import { API_URL } from '@env';
 import axios from 'axios';
 import { useCallback, useRef } from 'react';
 import { axiosClient } from '../utils/axiosClient';
+import { useAufgussAudio } from './useAufgussAudio';
 
 export const useAufguss = () => {
   const requested = useRef<boolean>(false);
+  const { playSound, stopSound } = useAufgussAudio();
 
   const start = useCallback(async () => {
     try {
+      console.log(API_URL);
       await axiosClient.post<{ message: string }>(`${API_URL}blower/start`);
+      await playSound();
       console.log('start');
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -17,11 +21,13 @@ export const useAufguss = () => {
       }
       console.error('エラーが発生しました。');
     }
-  }, []);
+  }, [playSound]);
 
   const stop = useCallback(async () => {
     try {
+      console.log(API_URL);
       await axiosClient.post<{ message: string }>(`${API_URL}blower/stop`);
+      stopSound();
       console.log('stop');
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -30,7 +36,7 @@ export const useAufguss = () => {
       }
       console.error('エラーが発生しました。');
     }
-  }, []);
+  }, [stopSound]);
 
   const startAufguss = useCallback(async () => {
     try {
