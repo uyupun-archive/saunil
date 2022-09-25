801d68c522d6991d3c26ca490f0723b8ecf0da85
suzuking1125 <b7165@oic.jp>
2022-09-25 12:32:04 +0900

    fix for eslint

--- src/pages/top/index.tsx
+++ src/pages/top/index.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { StackNavigator } from '../../types/stackNavigator';
-import { Button } from '@rneui/themed';
+//import { TopPageBg } from '../../assets/topPageBG.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    //backgroundImage: TopPageBg,
+    backgroundColor: '#FAB060',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 70,
+    width: 220,
+    fontSize: 60,
+    fontWeight: 'bold',
+    color: '#000000',
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginBottom: 169,
+  },
+  modeButton: {
+    marginBottom: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 50,
+    backgroundColor: '#ffffff',
+    width: 210,
+    height: 50,
+    borderColor: 'black',
+    borderWidth: 3,
+  },
+  buttonText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#000000',
   },
 });
 
@@ -20,15 +54,22 @@ export const Top = () => {
 
   return (
     <View style={styles.container}>
-      <Text>トップページ</Text>
-      <Button
-        title="アウフグースモードへ"
+      <Text style={styles.title}>SANNIL</Text>
+      <Text style={styles.subtitle}>
+        サウナはもう要らない　ここがサウナだから
+      </Text>
+      <Pressable
+        style={styles.modeButton}
         onPress={() => navigation.navigate('Aufguss')}
-      />
-      <Button
-        title="ロウリュモードへ"
+      >
+        <Text style={styles.buttonText}>アウフグースモードへ</Text>
+      </Pressable>
+      <Pressable
+        style={styles.modeButton}
         onPress={() => navigation.navigate('Loyly')}
-      />
+      >
+        <Text style={styles.buttonText}>ロウリュモードへ"</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
